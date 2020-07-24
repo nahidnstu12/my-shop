b7925eae1c690397c9e85610a107cd207bca8a58
= <=>
2020-07-24 06:30:48 +0600

    Updates

---
+++ precache-manifest.2b46671918d1dc4ad8b96d3dde9127c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8be6de91fc92e94ba9eb08c8cdc1a7a",
+    "url": "/my-store/index.html"
+  },
+  {
+    "revision": "02d48b435cd027e427af",
+    "url": "/my-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6389ee3cf27d28af98f4",
+    "url": "/my-store/static/css/main.2e0bf199.chunk.css"
+  },
+  {
+    "revision": "02d48b435cd027e427af",
+    "url": "/my-store/static/js/2.2bae911e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-store/static/js/2.2bae911e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6389ee3cf27d28af98f4",
+    "url": "/my-store/static/js/main.5ddfd263.chunk.js"
+  },
+  {
+    "revision": "dede5a0dafd8c15fb0f3",
+    "url": "/my-store/static/js/runtime-main.2baada54.js"
+  }
+]);
