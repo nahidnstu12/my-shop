359613e39fa4bd6179a1b21f977f2aa432be4c89
= <=>
2020-07-24 06:59:03 +0600

    testing

--- src/Carts/Checkout.js
+++ src/Carts/Checkout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Checkout = () =>{
     return(
         <div>
-            <h3>Check out</h3>
+            <h3>Check out-this is testing</h3>
         </div>
     )
 }
